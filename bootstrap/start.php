149d86d1b59d138f317f2399dbaccd16b7911751
Eugene_H <evzhik@gmail.com>
2014-10-23 18:10:10 +0400

    production env fix 2

--- bootstrap/start.php
+++ bootstrap/start.php
@@ -25,7 +25,7 @@ $app = new Illuminate\Foundation\Application;
 */
 
 $env = $app->detectEnvironment(array(
-    'production' => ['*.me', '*.tmweb.ru'],
+    'production' => ['*.ru'],
     'local' => ['*.dev', gethostname()]
 ));
 
