789a0a88f5b63a1e25a6e5c14c7b3931d74109b5
Eugene_H <evzhik@gmail.com>
2014-10-23 17:10:34 +0400

    intervention/image added

--- app/config/app.php
+++ app/config/app.php
@@ -120,7 +120,8 @@ return array(
 		'Illuminate\Translation\TranslationServiceProvider',
 		'Illuminate\Validation\ValidationServiceProvider',
 		'Illuminate\View\ViewServiceProvider',
-		'Illuminate\Workbench\WorkbenchServiceProvider'
+		'Illuminate\Workbench\WorkbenchServiceProvider',
+        'Intervention\Image\ImageServiceProvider'
 	),
 
 
@@ -188,6 +189,7 @@ return array(
 		'URL'             => 'Illuminate\Support\Facades\URL',
 		'Validator'       => 'Illuminate\Support\Facades\Validator',
 		'View'            => 'Illuminate\Support\Facades\View',
+        'Image'           => 'Intervention\Image\Facades\Image'
 
 	),
 
